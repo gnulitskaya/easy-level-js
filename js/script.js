8ba214832c0d6732947671f085699d581dca40ba
Lera <webgnule@mail.ru>
2021-07-21 16:03:45 +0300

    add loops

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 let numberOfFilms = prompt("Сколько фильмов вы посмотрели?");
-let questionFirst = prompt("Один из последних просмотренных фильмов?");
-let questionSecond = prompt("На сколько оцените его?");
+
 
 let personalMovieDB = {
 	count: numberOfFilms,
@@ -10,6 +9,29 @@ let personalMovieDB = {
 	privat: false
 };
 
-personalMovieDB.movies[questionFirst] = questionSecond;
+for (let i = 0; i < 2; i++) {
+	const questionFirst = prompt("Один из последних просмотренных фильмов?"),
+		questionSecond = prompt("На сколько оцените его?");
+
+	if (questionFirst != '' && questionSecond != '' && questionFirst != null &&
+		questionSecond != null && questionFirst.length < 50) {
+		console.log('done');
+		personalMovieDB.movies[questionFirst] = questionSecond;
+	} else {
+		console.log('error');
+		i--;
+	}
+
+}
+
+if (personalMovieDB.count < 10) {
+	console.log("Мало мало ТЕБя");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+	console.log("средний БОЕЦ");
+} else if (personalMovieDB.count >= 30) {
+	console.log("киноМАН");
+} else {
+	console.log("error");
+}
 
 console.log(personalMovieDB);
