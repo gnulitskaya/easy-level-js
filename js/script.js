86656c6dd19ccec307cafd81bd59de26baa44e20
Lera <webgnule@mail.ru>
2021-07-25 16:38:26 +0300

    object commit

--- js/script.js
+++ js/script.js
@@ -1,68 +1,97 @@
-let numberOfFilms = prompt("Сколько фильмов вы посмотрели?");
 
-function start() {
-	let numberOfFilms = +prompt("Сколько фильмов вы посмотрели?", '');
-
-	while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-		numberOfFilms = +prompt("Сколько фильмов вы посмотрели?", '');
-	}
-}
-
-start();
 
 let personalMovieDB = {
-	count: numberOfFilms,
+	count: 0,
 	movies: {},
 	actors: {},
 	genres: [],
-	privat: false
+	privat: false,
+	toggleVisibleMyBD: function () {
+		if (personalMovieDB.privat) {
+			personalMovieDB.privat = false;
+		} else {
+			personalMovieDB.privat = true;
+		}
+	},
+	start: function () {
+		personalMovieDB.count = +prompt("Сколько фильмов вы посмотрели?", '');
+
+		while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+			personalMovieDB.count = +prompt("Сколько фильмов вы посмотрели?", '');
+		}
+	},
+	rememberMyFilms: function () {
+		for (let i = 0; i < 2; i++) {
+			const questionFirst = prompt("Один из последних просмотренных фильмов?"),
+				questionSecond = prompt("На сколько оцените его?");
+
+			if (questionFirst != '' && questionSecond != '' && questionFirst != null &&
+				questionSecond != null && questionFirst.length < 50) {
+				console.log('done');
+				personalMovieDB.movies[questionFirst] = questionSecond;
+			} else {
+				console.log('error');
+				i--;
+			}
+
+		}
+	},
+	detectPersonalLevel: function () {
+		if (personalMovieDB.count < 10) {
+			console.log("Мало мало ТЕБя");
+		} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+			console.log("средний БОЕЦ");
+		} else if (personalMovieDB.count >= 30) {
+			console.log("киноМАН");
+		} else {
+			console.log("error");
+		}
+	},
+	showMyBD: function (hidden) {
+		if (!hidden) {
+			console.log(personalMovieDB);
+		}
+	},
+	writeYourGenres: function () {
+		for (let i = 1; i < 2; i++) {
+			let nameOfGenres = prompt(`Введите любимые жанры через запятую`).toLowerCase();
+			if (nameOfGenres == '' || nameOfGenres == null) {
+				i--;
+			} else {
+				personalMovieDB.genres = nameOfGenres.split(',');
+				personalMovieDB.genres.sort();
+			}
+
+		}
+		personalMovieDB.genres.forEach((item, i) => {
+			console.log(`Любимый жанр ${i + 1} - это ${item}`);
+		});
+	}
 };
 
-function rememberMyFilms() {
-	for (let i = 0; i < 2; i++) {
-		const questionFirst = prompt("Один из последних просмотренных фильмов?"),
-			questionSecond = prompt("На сколько оцените его?");
+// personalMovieDB.start();
+// personalMovieDB.toggleVisibleMyBD();
+// personalMovieDB.rememberMyFilms();
+// personalMovieDB.detectPersonalLevel();
+// personalMovieDB.showMyBD(personalMovieDB.privat);
+// personalMovieDB.writeYourGenres();
 
-		if (questionFirst != '' && questionSecond != '' && questionFirst != null &&
-			questionSecond != null && questionFirst.length < 50) {
-			console.log('done');
-			personalMovieDB.movies[questionFirst] = questionSecond;
-		} else {
-			console.log('error');
-			i--;
-		}
 
-	}
-}
+// const options = {
+// 	name: 'lera',
+// 	width: 1024,
+// 	colors: {
+// 		border: 'red',
+// 		background: 'yellow'
+// 	}
+// };
 
-rememberMyFilms();
-
-function detectPersonalLevel() {
-	if (personalMovieDB.count < 10) {
-		console.log("Мало мало ТЕБя");
-	} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-		console.log("средний БОЕЦ");
-	} else if (personalMovieDB.count >= 30) {
-		console.log("киноМАН");
-	} else {
-		console.log("error");
-	}
-}
-
-detectPersonalLevel();
-
-function showMyBD(hidden) {
-	if (!hidden) {
-		console.log(personalMovieDB);
-	}
-}
-
-showMyBD(personalMovieDB.privat);
-
-function writeYourGenres() {
-	for (let i = 1; i <= 3; i++) {
-		personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i} `);
-	}
-}
-
-writeYourGenres();
+// for (let key in options) {
+// 	if (typeof (options[key]) === 'object') {
+// 		for (let i in options[key]) {
+// 			console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+// 		}
+// 	} else {
+// 		console.log(`Свойство ${key} имеет значение ${options[key]}`);
+// 	}
+// }
